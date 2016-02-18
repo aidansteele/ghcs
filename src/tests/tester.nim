212dae162d31746aafb7dea8a312289c07fdeb1c
Aidan Steele <aidan.steele@glassechidna.com.au>
2016-02-18 12:31:11 +1100

    don't need this now

--- src/tests/tester.nim
+++ src/tests/tester.nim
@@ -1,6 +1,2 @@
 import test_duktape
 import test_rubocop
-
-# TODO: honestly this belongs anywhere else
-proc readJavascriptSource*(name: cstring, babelify: cint): cstring {.exportc.} =
-  result = readFile($name)
