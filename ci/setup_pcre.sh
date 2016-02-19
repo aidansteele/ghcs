bcc04741b9c2d02dcfd5474a7909ad34eb6b423e
Aidan Steele <aidan.steele@glassechidna.com.au>
2016-02-20 08:51:20 +1100

    travis futzing

--- ci/setup_pcre.sh
+++ ci/setup_pcre.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-if [ ! -d "$HOME/libpcre" ]; then
+if [ ! -d "$HOME/libpcre/lib" ]; then
   cd $HOME
   wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.bz2
   tar -jxf pcre-8.38.tar.bz2
