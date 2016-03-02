e4991cb1dcc1b6ff710e897ceb2200c1a44df2f6
Aidan Steele <aidan.steele@glassechidna.com.au>
2016-03-02 17:08:55 +1100

    Gemfile security vuln auditing

--- src/bundled_js.nim
+++ src/bundled_js.nim
@@ -10,7 +10,9 @@ macro bundleUpJavascript(): expr =
   for file in files:
      let src = """, "$1": staticExec("nim c -r --verbosity:0 babelify.nim 2>/dev/null", staticRead("js/$1"), "0.1")""" % [file]
      r = r & src
-  r = r & """, "underscore.js": staticRead("vendor/underscore/underscore.js")"""
+  r = r & """
+    , "underscore.js": staticRead("vendor/underscore/underscore.js")
+  """
   r = r & "}.toTable"
   result.add(parseStmt(r))
 
--- src/js/gemfile.js
+++ src/js/gemfile.js
@@ -1,39 +1,105 @@
 const Ghcs = require('ghcs');
-const parser = require('gemfileParser');
 const _ = require('underscore');
 
+const rubyCode = `
+    require 'bundler'
+    require 'yaml'
+    require 'json'
+
+    lockfile_path, vulns_path, *_ = ARGV
+
+    lockfile_parser = Bundler::LockfileParser.new File.read lockfile_path
+    all_deps = lockfile_parser.specs.map {|spec| [spec.name, spec.version ] }.to_h
+
+    all_vulns = Dir["#{vulns_path}/gems/*/**.yml"].map do |path|
+      YAML.load_file path
+    end
+
+    applicable_vulns = all_vulns.select do |vuln|
+      patched = vuln['patched_versions'] || []
+      unaffected = vuln['unaffected_versions'] || []
+      safe_versions = (patched + unaffected).map do |ver|
+        preds = ver.split(', ')
+        Gem::Requirement.new(*preds)
+      end
+
+      gem_name = vuln['gem']
+      gem_version = all_deps[gem_name]
+      next if gem_version.nil?
+
+      is_vuln = safe_versions.none? { |req| req === gem_version }
+      vuln['lockfile_version'] = gem_version.to_s if is_vuln
+      is_vuln
+    end
+
+    puts applicable_vulns.to_json
+`;
+
 export default class Gemfile {
-    constructor({context = 'gemfile', directory = '.'}) {
+    constructor({context = 'gemfile', directory = '.', description = false}) {
         this.context = context;
         this.directory = directory;
+        this.description = description;
     }
 
     run() {
-        this.vulnerabilities();
         return {
-            status: {},
-            metadata: { dependencies: this.dependencies() },
+            status: this.status(),
+            metadata: this.metadata(),
             comments: {}
         };
     }
 
-    lockfile() {
-        let lockfilePath = this.directory + '/Gemfile.lock';
-        let lockfileData = Ghcs.readFile({ path: lockfilePath });
-        return parser.parse(lockfileData);
+    metadata() {
+        let vulnerabilities = this.vulnerableGems();
+        return { vulnerabilities };
     }
 
-    dependencies() {
-        let specs = this.lockfile().GEM.specs;
-        let pairs = _.map(specs, (data, dep) => [dep, data.version]);
-        return _.object(pairs);
+    status() {
+        let vg = this.vulnerableGems();
+        var description;
+        var state;
+
+        if (vg.length == 0) {
+            state = 'success';
+            description = 'No gems with known vulnerabilities';
+        } else {
+            state = 'failure';
+            description = `${vg.length} gems with known vulnerabilities`;
+        }
+
+        return {
+            state,
+            description,
+            target_url: '',
+            context: this.context
+        };
     }
 
-    vulnerabilities() {
-        const url = 'https://codeload.github.com/rubysec/ruby-advisory-db/zip/master';
-        const path = 'ruby-advisory-db.zip';
-        let data = Ghcs.http({ url, method: 'GET' }).body;
-        Ghcs.writeFile({ path, data });
+
+    vulnerableGems() {
+        if (!this.rubyOutputJson) {
+            let advisoryPath = this.retrieveAdvisoryDb();
+
+            Ghcs.shell({ command: 'touch Gemfile' }); // todo: this shouldn't be necessary
+
+            var command = `ruby - ${this.directory}/Gemfile.lock ruby-advisory-db-master/`;
+            let rawOutput = Ghcs.shell({ command: command, stdin: rubyCode });
+            this.rubyOutputJson = JSON.parse(rawOutput);
+
+            if (!this.description) {
+                _.each(this.rubyOutputJson, (v) => delete v.description );
+            }
+        }
+
+        return this.rubyOutputJson;
+    }
+
+    retrieveAdvisoryDb() {
+        const url = 'https://github.com/rubysec/ruby-advisory-db/archive/master.tar.gz';
+        let command = `curl -sL ${url} | tar xz`;
+        Ghcs.shell({ command });
+        return 'ruby-advisory-db-master/';
     }
 }
 
