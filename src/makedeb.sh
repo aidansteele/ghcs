2c97e4e35e1422f708f203fc999db10ea37a625b
Aidan Steele <aidan.steele@rea-group.com>
2016-03-08 16:48:12 +1100

    travis test making a deb

---
+++ src/makedeb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+BASEDIR=$(dirname $0)
+FPMDIR="$BASEDIR/fpm-temp"
+BINDIR="$FPMDIR/usr/bin"
+MANDIR="$FPMDIR/usr/share/man/man1"
+
+mkdir -p $BINDIR
+mkdir -p $MANDIR
+
+cp "ghcs" "$BINDIR/"
+# copy the man file too
+
+rm "ghcs_1.0_amd64.deb"
+fpm -s dir -t deb -n ghcs -C $FPMDIR -d "openssl" .
+rm -rf $FPMDIR
