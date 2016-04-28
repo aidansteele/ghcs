715db5ff9aa81481be1a301f806f839026627153
Aidan Steele <aidan.steele@glassechidna.com.au>
2016-04-28 19:30:24 +1000

    flesh out git config (and new testing structure)

--- nim/git_config.nim
+++ nim/git_config.nim
@@ -2,6 +2,7 @@ import osproc
 import uri
 import strutils
 import os
+import nre
 
 type
   GitConfig* = tuple
@@ -15,16 +16,24 @@ proc normalisedRemoteUri(remote: string): Uri =
   if not (startsWith(remote, "http") or startsWith(remote, "ssh")):
     rem = "ssh://" & rem
   
-  if startsWith(remote, "ssh"):
-    rem = replace(rem, ":", "/")
+  if startsWith(rem, "ssh"):
+    rem = replace(rem, re"ssh://([^:]+):", "ssh://$1/")
   
   rem = replace(rem, "ssh://", "https://")  
   result = parseUri(rem)
   
 proc extractBaseAndRepo(remote: string): tuple[base: Uri, repo: string] =
   var uri = normalisedRemoteUri(remote)
-  let repoName = uri.path[1..len(uri.path)]
+  var repoName = uri.path[1..len(uri.path)]
+  removeSuffix(repoName, ".git")
+  
   uri.path = "/"
+  uri.username = ""
+  
+  # public github special case!
+  if uri.hostname == "github.com":
+    uri.hostname = "api.github.com"
+  
   result = (uri, repoName)
 
 proc defaultConfig*(): GitConfig =
@@ -46,4 +55,25 @@ proc envConfig*(): GitConfig =
   let url = getEnv("GHCS_API_URL")
   
   let it = extractBaseAndRepo(remote)
-  result = (repoName: it.repo, baseUri: parseUri(url), sha: sha)  
+  result = (repoName: it.repo, baseUri: parseUri(url), sha: sha)
+  echo result
+  
+when isMainModule or defined(testing):
+  import unittest
+  suite "git config tests":
+    test "handles public github special case":
+      let remote = "https://github.com/aidansteele/ghcs.git"
+      let it = extractBaseAndRepo(remote)
+      check(it.base == parseUri("https://api.github.com/"))
+  
+    test "splits up ssh url without scheme":
+      let remote = "git@github.com:aidansteele/ghcs.git"
+      let it = extractBaseAndRepo(remote)
+      check(it.repo == "aidansteele/ghcs")
+      
+    test "splits up ssh url with scheme":
+      let remote = "ssh://git@github.com:aidansteele/ghcs.git"
+      let it = extractBaseAndRepo(remote)
+      check(it.repo == "aidansteele/ghcs")
+    
+    # TODO: test if setting api envvar (but not other envvars) works, e.g. GHE
