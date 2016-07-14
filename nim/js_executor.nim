4bd8e9a4d08969595e04ec6b65d1b567c767f488
Aidan Steele <aidan.steele@glassechidna.com.au>
2016-07-15 08:22:59 +1200

    ghcs js engine is now a vtable of closures rather than hardcoded

--- nim/js_executor.nim
+++ nim/js_executor.nim
@@ -3,10 +3,23 @@ import nimduktape
 import bundled_js
 import json
 import ghcs_native_js
+import macros
 
 type
+  JsCallback* = proc(json: JsonNode): JsonNode
+  JsCallbackTable* = object # TODO: it'd be nicer if this was a table
+    readJavascriptSourceJson*: JsCallback
+    ghcsReadFile*: JsCallback
+    ghcsWriteFile*: JsCallback
+    ghcsStdin*: JsCallback
+    ghcsStdout*: JsCallback
+    ghcsShell*: JsCallback
+    ghcsHttp*: JsCallback
+    ghcsArgv*: JsCallback
+
   JsExecutor* = ref object
     context: DuktapeContext
+    callbacks: JsCallbackTable
 
 proc readJavascriptSource*(name: string, babelify: bool): JS =
   let bundledName = name & ".js"
@@ -22,7 +35,7 @@ proc readJavascriptSource*(name: string, babelify: bool): JS =
 
   result = JS(src)
 
-proc readThunkJson(json: JsonNode): JsonNode =
+proc readJavascriptSourceJson(json: JsonNode): JsonNode =
   let name = json["name"].str
   let src = readJavascriptSource(name, false)
   result = %*{ "src": string(src) }
@@ -39,25 +52,44 @@ proc execSourceFile*(jsExe: JSExecutor, name: string, babelify = false) =
   #let src = readJavascriptSource(name, babelify)
   #discard evalJavascript(jsExe.context, src)
 
-proc injectHelperFuncs(jsExe: JSExecutor) =
-  registerProc(jsExe.context, "_readJavascriptSourceJson", readThunkJson)
-  registerProc(jsExe.context, "_ghcsReadFile", ghcsReadFile)
-  registerProc(jsExe.context, "_ghcsWriteFile", ghcsWriteFile)
-  registerProc(jsExe.context, "_ghcsStdin", ghcsStdin)
-  registerProc(jsExe.context, "_ghcsStdout", ghcsStdout)
-  registerProc(jsExe.context, "_ghcsShell", ghcsShell)
-  registerProc(jsExe.context, "_ghcsHttp", ghcsHttp)
-  registerProc(jsExe.context, "_ghcsArgv", ghcsArgv)
+proc defaultJsCallbacks(): JsCallbackTable =
+  result = JsCallbackTable(
+    readJavascriptSourceJson: readJavascriptSourceJson, 
+    ghcsReadFile: ghcsReadFile,
+    ghcsWriteFile: ghcsWriteFile,
+    ghcsStdin: ghcsStdin,
+    ghcsStdout: ghcsStdout,
+    ghcsShell: ghcsShell,
+    ghcsHttp: ghcsHttp,
+    ghcsArgv: ghcsArgv
+  )
+
+macro stringify(n: expr): string =
+  result = newNimNode(nnkStmtList, n)
+  result.add(toStrLit(n))
+
+template injectHelper(jsExe, cbt, def, name: expr) =
+  let op = if isNil(cbt.`name`): def.`name` else: cbt.`name`
+  registerProc(jsExe.context, "_" & stringify(`name`), op) 
+
+proc injectHelperFuncs(jsExe: JSExecutor, cb: JsCallbackTable) =
+  let def = defaultJsCallbacks()
+  injectHelper(jsExe, cb, def, readJavascriptSourceJson)
+  injectHelper(jsExe, cb, def, ghcsReadFile)
+  injectHelper(jsExe, cb, def, ghcsWriteFile)
+  injectHelper(jsExe, cb, def, ghcsStdin)
+  injectHelper(jsExe, cb, def, ghcsStdout)
+  injectHelper(jsExe, cb, def, ghcsShell)
+  injectHelper(jsExe, cb, def, ghcsHttp)
+  injectHelper(jsExe, cb, def, ghcsArgv)
 
   let loader = readJavascriptSource("moduleLoader", false)
   discard evalJavascript(jsExe.context, loader)
-  #execSourceFile(jsExe, "moduleLoader")
 
-proc newJsExecutor*(): JsExecutor =
-  let ctx = createNewContext()
-  let jsExe = JsExecutor(context: ctx)
-  injectHelperFuncs(jsExe)
-
+proc newJsExecutor*(callbacks: JsCallbackTable = JsCallbackTable()): JsExecutor =
+  let ctx = createNewContext()  
+  let jsExe = JsExecutor(context: ctx, callbacks: callbacks)
+  injectHelperFuncs(jsExe, callbacks)
   result = jsExe
 
 # TODO: where are the non-experimental destructors?
